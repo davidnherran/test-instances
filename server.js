4521a21cac3d4d9b4ad41118573b082e323403cc
davidnherran <davidnherran@gmail.com>
2022-03-16 20:50:35 -0500

    change port

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Server is running'));
 
-app.listen(443, () => console.log('🚀 Server is running'));
+app.listen(8080);
+console.log("🚀 Server running at http://localhost:8080/");
